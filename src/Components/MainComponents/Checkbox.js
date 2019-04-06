02a3fc495e76456080e6d2782b1c79056f17c20b
Israa <israa96sulaiman@gmail.com>
2019-04-06 16:57:26 +0300

    days page style

--- src/Components/MainComponents/Checkbox.js
+++ src/Components/MainComponents/Checkbox.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default (props) => {
   const {labelClass, inputClass, name} = props;
   return (
-    <>
+    <div className ='item--container'>
     <label className={labelClass} htmlFor={name}>{name}</label>
-    <input name={name} value={name} className={inputClass} id={name} type='checkbox'></input>
-    </>
+    <input name={name} value={name} className={inputClass} id={name} type='radio'></input>
+    </div>
   )
 }
