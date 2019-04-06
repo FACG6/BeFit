25cb94129674fb5349e1352291bafcd459cd0111
Israa <israa96sulaiman@gmail.com>
2019-04-06 16:26:47 +0300

    made initial style for home case 1

--- src/Components/MainComponents/Welcome.js
+++ src/Components/MainComponents/Welcome.js
@@ -7,7 +7,7 @@ export default () => {
     <div className='welcome--container'>
       <h2 className='welcome--heading'>Hello Mrs. Farah</h2>
       <p className='welcome--content'>Start managing your exercises by adding a schedule</p>
-     <Link to='/select-days'> <FontAwesomeIcon icon='plus-circle' /></Link>
+     <Link to='/select-days'> <FontAwesomeIcon className='add--icon' icon='plus-circle' /></Link>
     </div>
   )
 }
