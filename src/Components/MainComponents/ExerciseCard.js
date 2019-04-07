08abba1906225637cb361805fb2c2d1dfe70011b
Israa <israa96sulaiman@gmail.com>
2019-04-07 13:09:23 +0300

    exercises style

--- src/Components/MainComponents/ExerciseCard.js
+++ src/Components/MainComponents/ExerciseCard.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Checkbox from '../MainComponents/Checkbox';
 
 export default function({ name, src }) {
     return (
     <div className='exercise_card' key={Date.now()}>
-       <span>{name}</span>
-       <img src={src} alt={name} />
-      <Checkbox htmlFor={name} label={null} name={name}/>
+       <span className='exercise-name'>{name}</span>
+       <img className='exercise-img' src={src} alt={name} />
+       <input name={name} className='exercise-checkbox' id={name} type='radio'></input>
     </div>
   )
 }
